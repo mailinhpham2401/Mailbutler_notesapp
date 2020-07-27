2fd19e36ce6ba616bd98f37840d3c7e64aa53fe9
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-07-27 11:46:57 +0200

    4

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,11 @@
     <h3 style="font-family:'Arial' bold;">Login</h3>
     <form @submit.prevent="login" action="">
       <div v-if="errors" class="errors">
-        <p v-for="(error, index) in errors" :key="index" >
+        <p v-for="(error, index) in errors" :key="index">
           {{ error }}
         </p>
       </div>
-      <input
-        type="text"
-        v-model="form.email"
-        placeholder="Email"
-      /><br />
+      <input type="text" v-model="form.email" placeholder="Email" /><br />
       <input
         type="password"
         v-model="form.password"
@@ -24,7 +20,7 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "Login",
   data() {
@@ -38,14 +34,15 @@ export default {
   },
   methods: {
     login() {
-			axios.post('https://beta.mailbutler.io/api/v2/users/login', this.form)
-		.then(response =>{
-			console.log(response);
-			this.$router.push('/');
-		});
+      axios
+        .post("https://beta.mailbutler.io/api/v2/users/login", this.form)
+        .then((response) => {
+          console.log(response);
+          this.$router.push("/");
+        });
+    },
   },
-}
-}
+};
 </script>
 
 <style lang="scss" scoped></style>
