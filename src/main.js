5972c5545cc06e26c72d9dc3a4306a813a834d0a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-07-27 10:17:06 +0200

    4

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { routes } from "./routes";
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
-  // mode: "history",
+  mode: "history",
 });
 
 Vue.config.productionTip = false;
